1fe999094cdd3d9098d1a118e1ecaeccb3c131b6
Chelita-collab <fg111120221413@gmail.com>
2026-05-18 16:58:34 -0600

    Este es el archivo principal
    Este archivo contiene acceso a los demás archivos

---
+++ index.php
@@ -0,0 +1,135 @@
+<?php
+echo "<head>";
+echo "<meta charset='UTF-8'>"; 
+echo "<title>Menú</title>";
+echo "</head>";
+echo "<body>";
+echo "<center>";
+echo "<h1>Menú de los ejercicios</h1>";
+echo "<br>";
+
+echo "<label>Ejercicio 1</label>";
+echo "<br>";
+echo "<a href='ejercicio1/form.html'>Siguiente</a>";
+echo "<br><br>";
+
+echo "<label>Ejercicio 2</label>";
+echo "<br>";
+echo "<a href='ejercicio2/form.html'>Siguiente</a>";
+echo "<br><br>";
+
+echo "<label>Ejercicio 3</label>";
+echo "<br>";
+echo "<a href='ejercicio3/form.html'>Siguiente</a>";
+echo "<br><br>";
+
+echo "<label>Ejercicio 4</label>";
+echo "<br>";
+echo "<a href='ejercicio4/form.html'>Siguiente</a>";
+echo "<br><br>";
+
+echo "<label>Ejercicio 5</label>";
+echo "<br>";
+echo "<a href='ejercicio5/form.html'>Siguiente</a>";
+echo "<br><br>";
+
+echo "<label>Ejercicio 6</label>";
+echo "<br>";
+echo "<a href='ejercicio6/form.html'>Siguiente</a>"; 
+echo "<br><br>";
+
+echo "<label>Ejercicio 7</label>";
+echo "<br>";
+echo "<a href='ejercicio7/form.html'>Siguiente</a>"; 
+echo "<br><br>";
+
+echo "<label>Ejercicio 8</label>";
+echo "<br>";
+echo "<a href='ejercicio8/form.html'>Siguiente</a>";
+echo "<br><br>";
+
+echo "<label>Ejercicio 9</label>";
+echo "<br>";
+echo "<a href='ejercicio9/form.html'>Siguiente</a>";
+echo "<br><br>";
+
+echo "<label>Ejercicio 10</label>";
+echo "<br>";
+echo "<a href='ejercicio10/form.html'>Siguiente</a>";
+echo "<br><br>";
+
+echo "<label>Ejercicio 11</label>";
+echo "<br>";
+echo "<a href='ejercicio11/form.html'>Siguiente</a>";
+echo "<br><br>";
+
+echo "<label>Ejercicio 12</label>";
+echo "<br>";
+echo "<a href='ejercicio12/form.html'>Siguiente</a>";
+echo "<br><br>";
+
+echo "<label>Ejercicio 13</label>";
+echo "<br>";
+echo "<a href='ejercicio13/form.html'>Siguiente</a>";
+echo "<br><br>";
+
+echo "<label>Ejercicio 14</label>";
+echo "<br>";
+echo "<a href='ejercicio14/form.html'>Siguiente</a>";
+echo "<br><br>";
+
+echo "<label>Ejercicio 15</label>";
+echo "<br>";
+echo "<a href='ejercicio15/form.html'>Siguiente</a>";
+echo "<br><br>";
+
+echo "<label>Ejercicio 16</label>";
+echo "<br>";
+echo "<a href='ejercicio16/form.html'>Siguiente</a>";
+echo "<br><br>";
+
+echo "<label>Ejercicio 17</label>";
+echo "<br>";
+echo "<a href='ejercicio17/form.html'>Siguiente</a>";
+echo "<br><br>";
+
+echo "<label>Ejercicio 18</label>";
+echo "<br>";
+echo "<a href='ejercicio18/form.html'>Siguiente</a>";
+echo "<br><br>";
+
+echo "<label>Ejercicio 19</label>";
+echo "<br>";
+echo "<a href='ejercicio19/form.html'>Siguiente</a>";
+echo "<br><br>";
+
+echo "<label>Ejercicio 20</label>";
+echo "<br>";
+echo "<a href='ejercicio20/form.html'>Siguiente</a>";
+echo "<br><br>";
+
+echo "<label>Ejercicio 21</label>";
+echo "<br>";
+echo "<a href='ejercicio21/form.html'>Siguiente</a>";
+echo "<br><br>";
+
+echo "<label>Ejercicio 22</label>";
+echo "<br>";
+echo "<a href='ejercicio22/form.html'>Siguiente</a>";
+echo "<br><br>";
+
+echo "<label>Ejercicio 23</label>";
+echo "<br>";
+echo "<a href='ejercicio23/form.html'>Siguiente</a>";
+echo "<br><br>";
+
+echo "<label>Ejercicio 24</label>";
+echo "<br>";
+echo "<a href='ejercicio24/form.html'>Siguiente</a>";
+echo "<br><br>";
+
+echo "<label>Ejercicio 25</label>";
+echo "<br>";
+echo "<a href='ejercicio25/form.html'>Siguiente</a>";
+echo "<br><br>";
+?>
