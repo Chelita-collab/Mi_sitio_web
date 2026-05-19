a60868cf808e65550fd223fbc600973f388f59c4
Chelita-collab <fg111120221413@gmail.com>
2026-05-18 22:40:31 -0600

    13. Ejercicio
    Diseñe un formulario que solicite el valor de un lado de un cuadrado y envíe la
    información a otro archivo PHP donde se calcule el perímetro.

---
+++ proc13.php
@@ -0,0 +1,10 @@
+<?php
+$lado = $_POST['lado'];
+$perimetro = $lado * 4;
+echo "<center>";
+echo "<h1>Cálculo del Perímetro</h1>";
+echo "<h2>El perímetro del cuadrado es: " . $perimetro . "</h2><br><br>";
+echo "<a href='form13.html'>Volver al formulario</a><br><br>";
+echo "<a href='index.html'>Volver al Menú</a>";
+echo "</center>";
+?>
