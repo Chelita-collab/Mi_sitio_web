26803b98fa05d20ae4b9dd69acd7a120602aa56c
Chelita-collab <fg111120221413@gmail.com>
2026-05-18 22:47:24 -0600

    20. Ejercicio
    Cree un formulario que permita ingresar un número y envíe el dato a otro archivo
    PHP donde se realice la suma de los números desde 1 hasta el número ingresado.

---
+++ proc20.php
@@ -0,0 +1,14 @@
+<?php
+$limite = $_POST['limite'];
+$suma = 0;
+for ($i = 1; $i <= $limite; $i++) {
+    $suma += $i;
+}
+echo "<center>";
+echo "<h1>Suma Consecutiva</h1>";
+echo "<h2>Número límite ingresado: " . $limite . "</h2>";
+echo "<h2>La suma total desde 1 hasta " . $limite . " es: " . $suma . "</h2><br><br>";
+echo "<a href='form20.html'>Volver al formulario</a><br><br>";
+echo "<a href='index.html'>Volver al Menú</a>";
+echo "</center>";
+?>
