8c7b8e6ef60ce83a0e184d7b1b14a228052d8f44
Chelita-collab <fg111120221413@gmail.com>
2026-05-18 22:44:26 -0600

    17. Ejercicio
    Cree un formulario que solicite el monto total de una compra y envíe la información
    a otro archivo PHP donde se aplique un descuento del 10% si la compra supera
    cierta cantidad.

---
+++ proc17.php
@@ -0,0 +1,16 @@
+<?php
+$monto = $_POST['monto'];
+$descuento = 0;
+if ($monto > 50) {
+    $descuento = $monto * 0.10;
+}
+$total = $monto - $descuento;
+echo "<center>";
+echo "<h1>Resumen de Compra</h1>";
+echo "<h2>Monto inicial: $" . number_format($monto, 2) . "</h2>";
+echo "<h2>Descuento aplicado: $" . number_format($descuento, 2) . "</h2>";
+echo "<h2>Total Neto a Pagar: $" . number_format($total, 2) . "</h2><br><br>";
+echo "<a href='form17.html'>Volver al formulario</a><br><br>";
+echo "<a href='index.html'>Volver al Menú</a>";
+echo "</center>";
+?>
