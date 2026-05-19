3a919d346152f96fbc51c0b2693b4a0a48ef9913
Chelita-collab <fg111120221413@gmail.com>
2026-05-18 22:39:25 -0600

    12. Ejercicio
    Elabore un formulario que solicite la base y la altura de un rectángulo y envíe los
    datos a otro archivo PHP donde se calcule y muestre el área.

---
+++ proc12.php
@@ -0,0 +1,11 @@
+<?php
+$base = $_POST['base'];
+$altura = $_POST['altura'];
+$area = $base * $altura;
+echo "<center>";
+echo "<h1>Cálculo del Área</h1>";
+echo "<h2>El área del rectángulo es: " . $area . "</h2><br><br>";
+echo "<a href='form12.html'>Volver al formulario</a><br><br>";
+echo "<a href='index.html'>Volver al Menú</a>";
+echo "</center>";
+?>
