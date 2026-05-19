f671860e250ec75b060f23fdb140a5cb32ca02ec
Chelita-collab <fg111120221413@gmail.com>
2026-05-18 22:34:43 -0600

    8. Ejercicio
    Cree un formulario que permita ingresar dos números y envíe la información a otro
    archivo PHP donde se indique cuál es el número menor.

---
+++ proc8.php
@@ -0,0 +1,16 @@
+<?php
+$n1 = $_POST['num1'];
+$n2 = $_POST['num2'];
+echo "<center>";
+if ($n1 < $n2) {
+    echo "<h2>El número menor es: " . $n1 . "</h2>";
+} elseif ($n2 < $n1) {
+    echo "<h2>El número menor es: " . $n2 . "</h2>";
+} else {
+    echo "<h2>Ambos números son iguales.</h2>";
+}
+echo "<br><br>";
+echo "<a href='form8.html'>Volver al formulario</a><br><br>";
+echo "<a href='index.html'>Volver al Menú</a>";
+echo "</center>";
+?>
