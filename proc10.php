76368834ccceeb8e875233ec7694b7dd0ab4f920
Chelita-collab <fg111120221413@gmail.com>
2026-05-18 22:37:10 -0600

    10. Ejercicio
    Diseñe un formulario que solicite tres notas de un estudiante y envíe la información
    a otro archivo PHP donde se calcule y muestre el promedio.

---
+++ proc10.php
@@ -0,0 +1,15 @@
+<?php
+$n1 = $_POST['nota1'];
+$n2 = $_POST['nota2'];
+$n3 = $_POST['nota3'];
+$promedio = ($n1 + $n2 + $n3) / 3;
+echo "<center>";
+echo "<h1>Resultado</h1>";
+echo "<h2>Nota 1: " . $n1 . "</h2>";
+echo "<h2>Nota 2: " . $n2 . "</h2>";
+echo "<h2>Nota 3: " . $n3 . "</h2>";
+echo "<h2>El promedio final es: " . round($promedio, 2) . "</h2><br><br>";
+echo "<a href='form10.html'>Volver al formulario</a><br><br>";
+echo "<a href='index.html'>Volver al Menú</a>";
+echo "</center>";
+?>
